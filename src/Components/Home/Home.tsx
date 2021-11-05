4007e3a7a40eceffced98f4d3c9f97ad12ff1cb8
Daniel <danielsoh123@gmail.com>
2021-11-05 19:19:16 +0800

    fix missing heading

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -3,10 +3,10 @@ import Container from "react-bootstrap/Container";
 const Home = () => {
 	return (
 		<Container className="header">
-			<div className="animate__animated animate__fadeInLeft animate__slow">
+			<div className="heading animate__animated animate__fadeInLeft animate__slow">
 				Hello, I'm <span>Daniel</span>.
 			</div>
-			<div className="animate__animated animate__fadeInRight animate__slow">I'm a web developer.</div>
+			<div className="heading animate__animated animate__fadeInRight animate__slow">I'm a web developer.</div>
 			<button className="header-button">
 				<a href="#about">
 					<i className="fas fa-angle-double-down fa-2x" />
