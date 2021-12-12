eeceb2ac989bfa962de7c32503a057e8d39c3dc1
Daniel <danielsoh123@gmail.com>
2021-12-13 00:03:41 +0800

    change header button

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -6,12 +6,12 @@ const Home = () => {
 			<div className="heading animate__animated animate__fadeInLeft animate__slow">
 				Hello, I'm <span>Daniel</span>.
 			</div>
-			<div className="heading animate__animated animate__fadeInRight animate__slow">I'm a web developer.</div>
-			<button className="header-button">
-				<a href="#about">
-					<i className="fas fa-angle-double-down fa-2x" />
-				</a>
-			</button>
+			<div className="heading animate__animated animate__fadeInRight animate__slow">
+				I'm a web developer.
+			</div>
+			<a href="#about" className="header-button animated-button">
+				<span>More</span>
+			</a>
 		</Container>
 	);
 };
